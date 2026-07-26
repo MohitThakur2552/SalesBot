c96fcd79a0dbfc07406bf5f35948c5ec1445a08b
Mohit Thakur <thakurmohit.0102@gmail.com>
2026-07-26 13:22:35 +0530

    updates for deployment

--- Backend/app.py
+++ Backend/app.py
@@ -1,21 +1,28 @@
-from flask import Flask, app
+from flask import Flask
 from flask_cors import CORS
+
 from routes.score import score_bp
 from routes.conversation import conversation_bp
 
-# from flask_cors import CORS
+app = Flask(__name__)
 
 CORS(
     app,
-    resources={r"/*": {"origins": [
-        "https://salesbot-front.onrender.com/"
-    ]}}
+    resources={
+        r"/*": {
+            "origins": [
+                "https://salesbot-front.onrender.com"
+            ]
+        }
+    }
 )
+
 app.register_blueprint(conversation_bp, url_prefix="/api")
-
-
-
 app.register_blueprint(score_bp, url_prefix="/api")
+
+@app.route("/")
+def home():
+    return {"status": "Backend is running"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
